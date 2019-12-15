0decb9c3a021d3f7420a2d579d010e29abc366fc
Abhijith Sreekar <abhijith.sreekar@infra.tigerspike.com>
2019-12-15 14:58:54 +0800

    Add options menu with default toast messages

--- app/src/main/java/com/abhijith/assignment/github_trending/MainActivity.kt
+++ app/src/main/java/com/abhijith/assignment/github_trending/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.abhijith.assignment.github_trending
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +13,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(findViewById(R.id.dashboard_toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here.
+        val id = item.itemId
+
+        if (id == R.id.sortByStars) {
+            Toast.makeText(this, "Sort By Stars Clicked", Toast.LENGTH_SHORT).show()
+            return true
+        }
+        if (id == R.id.sortByName) {
+            Toast.makeText(this, "Sort By Name Clicked", Toast.LENGTH_SHORT).show()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }

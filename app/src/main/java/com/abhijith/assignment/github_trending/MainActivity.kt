3ff88e0e6b81a55b5dccd024b193a7d976fe9eb2
Abhijith Sreekar <abhijith.sreekar@infra.tigerspike.com>
2019-12-15 14:08:45 +0800

    Initial Commit - Empty project created

---
+++ app/src/main/java/com/abhijith/assignment/github_trending/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.abhijith.assignment.github_trending
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

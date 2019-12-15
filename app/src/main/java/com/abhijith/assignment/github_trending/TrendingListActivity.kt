1b398b5c8dd640a1b9934a3dd4eded376de4d562
Abhijith Sreekar <abhijith.sreekar@infra.tigerspike.com>
2019-12-15 17:08:32 +0800

    Add Base activity for extension and rename main activity Added constants file with base url of github trending repo's Add more props for items in main_menu

--- app/src/main/java/com/abhijith/assignment/github_trending/TrendingListActivity.kt
+++ app/src/main/java/com/abhijith/assignment/github_trending/TrendingListActivity.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class TrendingListActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_trending_list)
 
         setSupportActionBar(findViewById(R.id.dashboard_toolbar))
     }
---
+++ app/src/main/java/com/abhijith/assignment/github_trending/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.abhijith.assignment.github_trending.util
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://github-trending-api.now.sh/repositories"
+    }
+}
